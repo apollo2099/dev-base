0b4412764d16ec82c0475e7899c4a9d616c6397c
liaoxiongjian <liaoxiongjian@iboxpay.com>
2016-01-13 15:22:28 +0800

    3.添加H+主题风格UI框架

--- src/main/java/com/demo/common/config/DemoConfig.java
+++ src/main/java/com/demo/common/config/DemoConfig.java
@@ -75,7 +75,7 @@ public class DemoConfig extends JFinalConfig {
 	 * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
 	 */
 	public static void main(String[] args) {
-//		JFinal.start("WebRoot", 80, "/", 5);
-		JFinal.start("src/main/webapp", 80, "/", 5);
+//		JFinal.start("WebRoot", 80, "/", 5);//传统项目集成方式启动
+		JFinal.start("src/main/webapp", 80, "/", 5);//maven集成方式启动
 	}
 }
